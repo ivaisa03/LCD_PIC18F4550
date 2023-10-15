e2bfe3c6d6db59ea29dc798bb536189758ef6bd3
ivaisa03 <147957255+ivaisa03@users.noreply.github.com>
2023-10-14 21:22:39 -0500

    Add files via upload

---
+++ LCD1602.c
@@ -0,0 +1,43 @@
+#include "LCD1602.h"
+
+void LCD_Init_Gpio(void)
+{
+    LCD_PUERTO_TRIS=0x00;//PARA QUE EL PUERTO SEA SALIDA
+    LCD_RS_TRIS=0;//SALIDA
+    LCD_RW_TRIS=0;//SALIDA
+    LCD_EN_TRIS=0;//SALIDA
+    
+    LCD_RW_LAT=0;//MODO ESCRITURA
+}
+void LCD_Send_Command(char comando)
+{
+    LCD_MODO_COMANDO();//RS=0
+    LCD_PUERTO_LAT=comando;
+    LCD_ENABLE_TOGGLE();
+}
+void LCD_Send_Character(char character)
+{
+    LCD_MODO_CARACTER();//RS=1
+    LCD_PUERTO_LAT=character;
+    LCD_ENABLE_TOGGLE();
+}
+void LCD_Init(void)
+{
+    LCD_Init_Gpio();//CONFIGURAMOS LOS PINES COMO SALIDA
+    LCD_Send_Command(0x01);//COMANDO: CLEAR DSIPLAY
+    LCD_Send_Command(0x02);//COMANDO: RETURN HOME
+    LCD_Send_Command(0x06);//COMANDO: I/D =1;DERECHA-SH=0;:INCREMENTO DERECHA SIN SHIFT
+    LCD_Send_Command(0x0F);//COMANDO: DISPLAY ON CURSOR ON Y BLINK ON 
+    LCD_Send_Command(0x38);//COMANDO: 8 BITS 2 LINEAS 5X8 CARACTER
+    LCD_Send_Command(0x02);//COMANDO: RETURN HOME
+    
+}
+void LCD_Send_String(char cadena[])
+{
+    uint8_t index = 0;
+    while(cadena[index]!='\0')
+    {
+        LCD_Send_Character(cadena[index]);
+        index++;
+    }
+}
---
+++ LCD1602.h
@@ -0,0 +1,42 @@
+/* 
+ * File:   LCD1602.h
+ * Author: Administrador
+ *
+ * Created on 10 de octubre de 2023, 03:55 PM
+ */
+
+#ifndef LCD1602_H
+#define	LCD1602_H
+
+#define _XTAL_FREQ 8000000UL
+
+#include<xc.h>
+#include<stdint.h>
+
+/*MACROS O DEFINICIONES*/
+
+#define LCD_PUERTO_LAT LATB
+#define LCD_PUERTO_TRIS TRISB
+
+#define LCD_RS_LAT LATCbits.LATC0 
+#define LCD_RW_LAT LATCbits.LATC1
+#define LCD_EN_LAT LATCbits.LATC2
+
+#define LCD_RS_TRIS TRISCbits.RC0
+#define LCD_RW_TRIS TRISCbits.RC1
+#define LCD_EN_TRIS TRISCbits.RC2
+
+#define LCD_MODO_COMANDO() LCD_RS_LAT = 0;//RS MODO COMADO O MODO CARACTER RS=0-> MODO COPMANDO
+#define LCD_MODO_CARACTER() LCD_RS_LAT = 1;//RS MODO COMADO O MODO CARACTER RS=1-> MODO CARACTER INFO QUE ENVIAS SE MUESTRA EN EL LCD
+#define LCD_ENABLE_TOGGLE() LCD_EN_LAT = 1; __delay_ms(5);LCD_EN_LAT=0; //SOLO CAUNDO SE ACTIVA EL ENABLE PASA LA INFORMACION SOLO CUANDO ES 1(SOLO SE ACTIVA UN PERIODO DE TIEMPO)
+
+
+/*FUNCIONES*/
+
+void LCD_Init_Gpio(void);
+void LCD_Send_Command(char comando);
+void LCD_Send_Character(char character);
+void LCD_Init(void);
+void LCD_Send_String(char cadena[]);
+#endif	/* LCD1602_H */
+
